143c8c87ec98501a63b9d7fb4b9bada164c82b06
Yulian-Caesar <yulik19971997@gmail.com>
2019-07-05 17:34:59 +0300

    add help section desctop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 
 //=include ../components/reviews/reviews.js
 //=include ../components/footer/footer.js
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
