5f164ed827dbd64f599353d1c187290122aeb0a0
Yulian-Caesar <yulik19971997@gmail.com>
2019-07-05 14:28:38 +0300

    add preference section and line before and after section

--- app/components/reviews/reviews.js
+++ app/components/reviews/reviews.js
@@ -4,7 +4,26 @@ $('.reviews .slider-for').slick({
     arrows: false,
     fade: true,
     infinite: true,
-    draggable: false
+    draggable: false,
+    swipe: false,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                adaptiveHeight: true
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                swipe: true,
+                adaptiveHeight: true,
+                prevArrow: '<button class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="40" viewBox="0 0 18 40"><path d="M7.983 0h2.034v.586L18 8.435l-1.439 1.414-6.544-6.435V40H7.983V3.414L1.438 9.849 0 8.435 7.983.585z"/></svg></button>',
+                nextArrow: '<button class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="40" viewBox="0 0 18 40"><path d="M7.983 0h2.034v.586L18 8.435l-1.439 1.414-6.544-6.435V40H7.983V3.414L1.438 9.849 0 8.435 7.983.585z"/></svg></button>'
+            }
+        }
+    ]
 });
 $('.reviews .slider-nav').slick({
     slidesToShow: 3,
@@ -13,6 +32,17 @@ $('.reviews .slider-nav').slick({
     infinite: true,
     asNavFor: '.slider-for',
     dots: true,
+    swipe: false,
     centerMode: true,
-    focusOnSelect: true
+    focusOnSelect: true,
+    prevArrow: '<button class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="40" viewBox="0 0 18 40"><path d="M7.983 0h2.034v.586L18 8.435l-1.439 1.414-6.544-6.435V40H7.983V3.414L1.438 9.849 0 8.435 7.983.585z"/></svg></button>',
+    nextArrow: '<button class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="40" viewBox="0 0 18 40"><path d="M7.983 0h2.034v.586L18 8.435l-1.439 1.414-6.544-6.435V40H7.983V3.414L1.438 9.849 0 8.435 7.983.585z"/></svg></button>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                vertical: false
+            }
+        }
+    ]
 });
